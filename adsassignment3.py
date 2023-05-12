493ccd5119048ad79180651da50088fda3a1e9ac
mohith2000 <130101357+mohith2000@users.noreply.github.com>
2023-05-12 17:36:37 +0100

    Add files via upload

--- adsassignment3.py
+++ adsassignment3.py
@@ -8,8 +8,8 @@ This is a temporary script file.
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import scipy.optimize as opt
 from sklearn.cluster import KMeans
-from scipy.optimize import curve_fit
 from scipy.stats import norm
 
 
@@ -43,6 +43,13 @@ def findxandy(df):
     return x_data, y_data, transposed_df, years
 
 
+def line(x, y, m, c):
+    """
+    """
+    y = m*x + c
+    return y
+
+
 # Load data for Aruba and Afghanistan
 df = pd.read_csv('API_19_DS2_en_csv_v2_5455435.csv', skiprows=4)
 # filter data for Aruba and Afghanistan and remove any NaN or inf values
@@ -60,43 +67,35 @@ x_data, y_data, trans_df, year = findxandy(df_filtered)
 # df.columns.name = None
 # df = df.rename(columns={'index': 'Year'})
 # Normalize data
-trans_df_norm = (trans_df.iloc[:, 1:] - trans_df.iloc[:, 1:].mean()) / trans_df.iloc[:, 1:].std()
-
 #######################################################################################
 
-
-# Get the list of all countries in the data
-countriesdata = transposeddata['Country Name'].unique()
-
-# Initialize an empty array to store the concatenated data
-concat_data1 = np.empty([0, 2])
-
-# Loop over the countries and concatenate the data for the two indicators and the years of interest
-for year in years:
-    data1 = transposeddata.loc[(transposeddata['Country Name'] == country) & (transposeddata['Indicator Name'] == indicator1name), datayears2].values[0]
-    data2 = transposeddata.loc[(transposeddata['Country Name'] == country) & (transposeddata['Indicator Name'] == indicator2name), datayears2].values[0]
-    data_country1 = np.column_stack((data1, data2))
-    concat_data1 = np.vstack([concat_data1, data_country1])
+trans_df.reset_index(inplace=True)
+trans_df.rename(columns={'index': 'Year'}, inplace=True)
+trans_df1 = trans_df[['Year', 'Energy use (kg of oil equivalent per capita)',
+                      'Urban population (% of total population)']]
 
 # Perform KMeans clustering with k=4
-kmeanscluster = KMeans(n_clusters= 4, random_state=0).fit(concat_data1)
+kmeanscluster = KMeans(n_clusters= 4, random_state=0).fit(trans_df1)
 
 # Get the cluster labels
 labels = kmeanscluster.labels_
 
+plt.figure(figsize=(10,6),dpi=500)
 # Set the plot title and axis labels
 plt.title('Total Population vs. Urban Population for all Countries (2000)')
 plt.xlabel('Population, total')
 plt.ylabel('Urban population')
 
 # Create a scatter plot for each cluster with a different color and label
-for i in range(kmeanscluster.n_clusters):
-    plt.scatter(concat_data1[labels == i, 0], concat_data1[labels == i, 1], label='Cluster {}'.format(i+1))
+#for i in range(kmeanscluster.n_clusters):
 
+plt.scatter(trans_df1['Energy use (kg of oil equivalent per capita)'],
+            trans_df1['Urban population (% of total population)'],
+            c=labels, cmap='tab10')
+plt.ylim(30,100)
+plt.xlim(1100,5000)
 # Add a legend to the plot with the cluster labels
 plt.legend()
-plt.savefig('scatterplotads.png', dpi=300)
-# Display the plot
 plt.show()
 
 
@@ -105,49 +104,30 @@ plt.show()
 
 
 
+plt.scatter(trans_df['Electricity production from natural gas sources (% of total)'],
+            trans_df['Urban population (% of total population)'],
+               )
 
-
-# Perform KMeans clustering
-kmeans = KMeans(n_clusters=2)
-kmeans.fit(df_norm.iloc[:, 1:])
-clusters = kmeans.predict(df_norm.iloc[:, 1:])
-df_norm['cluster'] = clusters
-# Plot cluster membership and cluster centers
-fig, ax = plt.subplots(figsize=(10, 6))
-scatter = ax.scatter(df_norm.iloc[:, 1], df_norm.iloc[:, 2], c=clusters, cmap='coolwarm')
-ax.set_xlabel('Aruba Urban Population (% of total population)')
-ax.set_ylabel('Afghanistan Urban Population (% of total population)')
-ax.set_title('Urban Population (% of total population) Clusters')
-handles, labels = scatter.legend_elements()
-legend = ax.legend(handles, ['Cluster 1', 'Cluster 2'], loc='lower left', title='Clusters')
-ax.add_artist(legend)
-
-# Plot cluster centers
-centers = kmeans.cluster_centers_
-ax.scatter(centers[:, 0], centers[:, 1], marker='X', s=200, color='black')
-for i, c in enumerate(centers):
-    ax.annotate(f'Cluster {i+1}', (c[0], c[1]), xytext=(c[0]-0.05, c[1]+0.05), fontsize=12)
-    
-plt.show()
+x = trans_df['Electricity production from natural gas sources (% of total)']
+y = trans_df['Urban population (% of total population)']
 # fit a logistic function to the data for Aruba and Afghanistan
-from scipy.optimize import curve_fit
 
-def logistic(x, A, B, C, k):
-    return A / (1 + np.exp(-k*(x-C))) + B
 
-# filter data for Aruba and Afghanistan
-df_filtered = df[(df['Country Name'] == 'Aruba') | (df['Country Name'] == 'Afghanistan')]
 
-# get data for urban population as a percentage of total population
-x_data = np.array(df_filtered['Year'])
-y_data = np.array(df_filtered['Urban population (% of total population)'])
 
-# normalize data
-x_norm = (x_data - np.min(x_data)) / (np.max(x_data) - np.min(x_data))
-y_norm = (y_data - np.min(y_data)) / (np.max(y_data) - np.min(y_data))
+# # filter data for Aruba and Afghanistan
+# df_filtered = df[(df['Country Name'] == 'Aruba') | (df['Country Name'] == 'Afghanistan')]
+
+# # get data for urban population as a percentage of total population
+# x_data = np.array(df_filtered['Year'])
+# y_data = np.array(df_filtered['Urban population (% of total population)'])
+
+# # normalize data
+# x_norm = (x_data - np.min(x_data)) / (np.max(x_data) - np.min(x_data))
+# y_norm = (y_data - np.min(y_data)) / (np.max(y_data) - np.min(y_data))
 
 # fit the logistic function to the normalized data
-popt, pcov = curve_fit(logistic, x_norm, y_norm)
+popt, pcov = opt.curve_fit(line, x_norm, y_norm)
 
 # create a range of x values for the predicted data
 x_predict = np.linspace(0, 1, num=100)
